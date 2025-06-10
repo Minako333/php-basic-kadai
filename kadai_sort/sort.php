87a16fd3d1ba473c65a117fd24e9251972aa012d
Minako333 <mnk11375.c@icloud.com>
2025-06-10 23:51:44 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -10,23 +10,26 @@
     <p>
         <?php
         $nums = [15, 4, 18, 23, 10];
-        function sort_2way ($nums)
-        if ($nums) {
-            echo '昇順にソートします。';
-
-            // 改行する
-            echo '<br>';
-
-　　　　　　　sort($nums);
-
-        　　　} else {
-             echo '降順にソートします。';
-
-             // 改行する
-            echo '<br>';
-
-            rsort($nums);
-         }
+        function sort_2way ($array, $order){
+            $array = [, 4, 18, 23, 10];
+            if ($array) {
+                echo '昇順にソートします。';
+    
+                // 改行する
+                echo '<br>';
+    
+    　　　　　　　sort($nums);
+    
+            　　　} else {
+                 echo '降順にソートします。';
+    
+                 // 改行する
+                echo '<br>';
+    
+                rsort($nums);
+             }
+        }
+        
 
 
          //  配列$numbersの要素を1つずつ順番に出力する
